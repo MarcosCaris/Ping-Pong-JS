645c5c7f3d4d579a2791b96f4ee1763903eed40b
Marcos Caris <agustinmarcos17@gmail.com>
2023-06-13 06:12:57 -0300

    imgs-update

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,3 +1,16 @@
 Swal.fire({
 	title: 'Bienvenido',
+	width: 500,
+	color: '#FFF',
+	showClass: {
+		popup: 'animate__animated animate__jackInTheBox',
+	},
+	hideClass: {
+		popup: 'animate__animated animate__fadeOutDown',
+	},
+	background: '#020300 url(./imagenes/intro__icon.png)',
+	backdrop: `
+	url("./imagenes/banner_2.jpg")
+	no-repeat
+  `,
 });
